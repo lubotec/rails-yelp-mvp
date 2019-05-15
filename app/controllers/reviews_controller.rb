4c48f0ebc35b131bb77600c0275f7e106412c273
Lukas Bollinger <lukas.bollinger21@gmail.com>
2019-05-15 15:41:42 +0200

    Ex 2 finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
     @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save(review_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
   def review_params
     params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
